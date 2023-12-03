bdd276f446e455bc67946ee5931f6fbc7e225fc3
Anonymus1145 <Anonymus1145@yahoo.com>
2023-12-03 22:10:46 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# ListLinked
+# Linked List
+
+This project implements a basic linked list data structure in TypeScript. A linked list is a data structure consisting of a sequence of nodes, where each node stores a value and a reference to the next node in the list.
+
+## Features
+
+- `size()`: Returns the number of elements in the linked list.
+- `head()`: Returns the first node in the linked list.
+- `tail()`: Returns the last node in the linked list.
+- `prepend(value)`: Adds a new node with the given value at the beginning of the linked list.
+- `append(value)`: Adds a new node with the given value at the end of the linked list.
+- `at(index)`: Returns the node at the specified index in the linked list.
+- `pop()`: Removes the last node from the linked list.
+- `contains(value)`: Checks if the linked list contains a node with the given value.
+- `find(value)`: Returns the index of the first node with the given value in the linked list.
+- `toString()`: Returns a string representation of the linked list.
+
+## Usage
+
+1. Install dependencies: `npm install`
+2. Build the TypeScript code: `npm run build`
+3. Run the example code: `npm run start`
+
+```typescript
+import { LinkList } from './linkList';
+
+const myList = new LinkList();
+myList.append(5);
+myList.prepend(2);
+console.log(myList.toString()); // Output: 2 -> 5
+console.log(myList.size()); // Output: 2
+
+## Contributions
+
+Contributions are welcome! If you find any issues or want to add new features, feel free to submit a pull request.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ linklist.ts
@@ -0,0 +1,144 @@
+type LinkedList = {
+  size: () => number;
+  head: () => any;
+  tail: () => any;
+  prepend: (value: any) => void;
+  append: (value: any) => void;
+  at: (index: number) => any;
+  pop: () => void;
+  contains: (value: any) => boolean;
+  find: (value: any) => number;
+  toString: () => string;
+};
+
+function LinkList(this: LinkedList) {
+  let length: number = 0;
+  let head: any = null;
+  let tail: any = null;
+
+  let Node = function (this: any, value: any) {
+    this.value = value;
+    this.next = null;
+  };
+
+  this.size = function () {
+    return length;
+  };
+
+  this.head = function () {
+    return head;
+  };
+
+  this.tail = function () {
+    return tail;
+  };
+
+  this.prepend = function (value: any) {
+    let node: any = new (Node as any)(value);
+    if (head === null) {
+      head = node;
+    } else {
+      node.next = head;
+      head = node;
+    }
+    length++;
+  };
+
+  this.append = function (value: any) {
+    let node: any = new (Node as any)(value);
+    let currentNode;
+    if (head === null) {
+      head = node;
+    } else {
+      currentNode = head;
+
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+
+      currentNode.next = node;
+    }
+    length++;
+  };
+
+  this.at = function (index: number) {
+    let count = 0;
+    let currentNode = this.head();
+
+    while (count < index) {
+      currentNode = currentNode.next;
+      count++;
+    }
+
+    return currentNode;
+  };
+
+  this.pop = function () {
+    let currentNode = this.head();
+    let newTail = this.tail();
+
+    while (currentNode.next) {
+      newTail = currentNode;
+      currentNode = currentNode.next;
+    }
+    if (newTail !== null) {
+      newTail.next = null;
+    }
+    length--;
+  };
+
+  this.contains = function (value: any) {
+    let currentNode = this.head();
+
+    while (currentNode.next) {
+      if (currentNode.value === value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+  };
+
+  this.find = function (value: any) {
+    let currentNode = this.head();
+    let index = 0;
+
+    while (currentNode.next) {
+      if (currentNode.value === value) {
+        return index;
+      }
+      currentNode = currentNode.next;
+      index++;
+    }
+
+    return -1;
+  };
+
+  this.toString = function () {
+    let currentNode = head;
+    let string = "";
+
+    while (currentNode.next) {
+      string += currentNode.value + " -> ";
+      currentNode = currentNode.next;
+    }
+
+    return string += currentNode.value;
+  };
+}
+
+const myList = new (LinkList as any)();
+
+myList.append(5);
+myList.append(10);
+myList.prepend(15);
+
+
+console.log(myList.toString()); // 15 -> 5 -> 10
+console.log(myList.size()); // 3
+console.log(myList.at(2).value);  // 10
+
+myList.pop();
+console.log(myList.toString()); // 15 -> 5
+console.log(myList.contains(5)); // true
+
